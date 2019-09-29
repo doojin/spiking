13578def6154e09cef08118cc1d6001509156ae6
doojin <dmitry.papka@gmail.com>
2019-09-28 21:56:53 -0300

    creating sandbox for code execution

--- puppeteer-data-extractor/app.js
+++ puppeteer-data-extractor/app.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const { JSDOM } = require('jsdom');
+const vm = require('vm');
 
 const extract = async (url, extractor) => {
     const browser = await puppeteer.launch();
@@ -9,16 +10,21 @@ const extract = async (url, extractor) => {
 
     const currentUrl = page.url();
     const html = await page.content();
-    
+
     extractData(currentUrl, html, extractor);
 }
 
-const extractData = (url, content, extractor) => {
+const extractData = (url, content, extractorScript) => {
     const document = new JSDOM(content, { url }).window.document;
-    extractor(document);
+
+    const results = [];
+    const addResult = result => results.push(result);
+
+    const sandbox = { document, addResult };
+    vm.createContext(sandbox);
+    vm.runInContext(extractorScript, sandbox);
+
+    console.log(results);
 }
 
-extract('http://google.com', (document) => {
-    const divCount = document.querySelectorAll('div').length;
-    console.log(divCount);
-});
+extract('http://google.com', 'addResult(document.querySelectorAll("div").length)');
