e88a1e1b311953a0d16219d5c9543b6f85fa1d09
doojin <dmitry.papka@gmail.com>
2019-09-28 21:50:32 -0300

    passing function for data extraction

--- puppeteer-data-extractor/app.js
+++ puppeteer-data-extractor/app.js
@@ -1,14 +1,24 @@
 const puppeteer = require('puppeteer');
+const { JSDOM } = require('jsdom');
 
-async function extract(url) {
+const extract = async (url, extractor) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.goto(url);
 
+    const currentUrl = page.url();
     const html = await page.content();
-
-    console.log(html);
+    
+    extractData(currentUrl, html, extractor);
 }
 
-extract('https://irc.lv');
+const extractData = (url, content, extractor) => {
+    const document = new JSDOM(content, { url }).window.document;
+    extractor(document);
+}
+
+extract('http://google.com', (document) => {
+    const divCount = document.querySelectorAll('div').length;
+    console.log(divCount);
+});
