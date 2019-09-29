d21d981a68a482a3585d3c9ce33abc47f41902cb
doojin <dmitry.papka@gmail.com>
2019-09-29 17:01:30 -0300

    example of sandboxed script

--- node-sandbox/app.js
+++ node-sandbox/app.js
@@ -1,6 +1,39 @@
-const vm = reuqire('vm');
+const vm = require("vm");
+const request = require("request");
 
 const data = [];
 
-const add = data.push;
-const getInfo =
+const add = obj => data.push(obj);
+const getInfo = () =>
+  new Promise((resolve, reject) => {
+    request(
+      "https://jsonplaceholder.typicode.com/posts/1",
+      (error, _, body) => {
+        return error ? reject(error) : resolve(body);
+      }
+    );
+  });
+
+const sandbox = {
+  api: {
+    add,
+    getInfo
+  }
+};
+vm.createContext(sandbox);
+
+const code = `
+  const x = 5;
+  const y = 8;
+
+  api.add(x + y);
+
+  api.getInfo().then(post => api.add(post));
+`;
+
+const run = async () => {
+    await vm.runInContext(code, sandbox);
+    console.log(data);
+};
+
+run();
